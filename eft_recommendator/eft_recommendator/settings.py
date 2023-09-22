6b8c999cbe3f8b7f5f1525278eb293d58c2a1626
David Velasquez <40569893+davidve0206@users.noreply.github.com>
2023-09-22 09:06:59 -0500

    Prepare for production on pythonanywhere

--- eft_recommendator/eft_recommendator/settings.py
+++ eft_recommendator/eft_recommendator/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = [Path.joinpath(BASE_DIR, 'static')]
 STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
 
 # Default primary key field type
